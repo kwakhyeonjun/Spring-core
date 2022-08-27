b1e7e41953c4bb951180d33e5590891259670cd2
kwakhyeonjun <kwak0568@naver.com>
2022-08-27 23:09:32 +0900

    feat: Component Scan

--- src/main/java/hello/core/member/repository/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/repository/MemoryMemberRepository.java
@@ -1,10 +1,12 @@
 package hello.core.member.repository;
 
 import hello.core.member.domain.Member;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     // 동시성 이슈 때문에 ConcurrentHashMap 사용함.
